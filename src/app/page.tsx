01fa1b04005a25430ee85a380739e428a85e2c62
Nick <melgish@users.noreply.github.com>
2024-06-02 16:08:51 -0400

    unnecessary precision (#2)
    * reduce precision in svg
    * format-on-save lies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default async function Home() {
       <header>
         <div>Home</div>
       </header>
-      <div className="grid grid-cols-2">
+      <div className="flex flex-wrap justify-around gap-y-2">
         {buckets.map((b) => (
           <div key={b.id}>
             <div>
--- src/components/BucketPHTrend.tsx
+++ src/components/BucketPHTrend.tsx
@@ -17,84 +17,84 @@ export default function BucketPHTrend({ bucket }: Props) {
   // Generate y axis ticks. [label, value]
   const yDomain = [5, 8]
   const yScale = d3.scaleLinear().domain(yDomain).range([height, 0])
-  const yTicks = yScale.ticks(9).map((d) => [d.toFixed(1), yScale(d)])
+  const yScale1 = (d: number) => +yScale(d).toFixed(1)
+  const yTicks = yScale.ticks(9).map((d) => [d.toFixed(1), yScale1(d)])
 
   // X scale changes based on measurement dates.
   // Generate x axis ticks. [label, value]
   const xDomain = d3.extent(data, (d) => d.createdAt!) as [Date, Date]
   const xScale = d3.scaleTime().domain(xDomain).range([0, width])
+  const xScale1 = (d: Date) => +xScale(d).toFixed(1)
   const timeFormat = xScale.tickFormat(8, '%b %d')
-  const xTicks = xScale.ticks(8).map((d) => [timeFormat(d), xScale(d)])
+  const xTicks = xScale.ticks(8).map((d) => [timeFormat(d), xScale1(d)])
 
   const line = d3
     .line<Reading>()
-    .x((d) => xScale(d.createdAt))
-    .y((d) => yScale(d.ph))
+    .x((d) => xScale1(d.createdAt))
+    .y((d) => yScale1(d.ph))
 
   let r = null
   if (bucket.plant) {
     // Calculate range bands based on plant min/max PH
     r = {
-      max0: yScale(bucket.plant.maxPH + 0.5),
-      max1: yScale(bucket.plant.maxPH),
-      min1: yScale(bucket.plant.minPH),
-      min0: yScale(bucket.plant.minPH - 0.5),
+      max0: yScale1(bucket.plant.maxPH + 0.5),
+      max1: yScale1(bucket.plant.maxPH),
+      min1: yScale1(bucket.plant.minPH),
+      min0: yScale1(bucket.plant.minPH - 0.5),
     }
   }
 
   return (
     <svg
-      className="ph-trend"
       width={width + margin.left + margin.right}
       height={height + margin.top + margin.bottom}
       fontFamily="sans-serif"
       fontSize={10}
+      stroke="currentColor"
+      textAnchor="middle"
     >
       <g transform={`translate(${margin.left},${margin.bottom})`}>
-        <g
-          className="x axis"
-          fill="none"
-          textAnchor="middle"
-          transform={`translate(0, ${height})`}
-        >
-          <path className="domain" d={`M0,6V0H${width}V6`} stroke="currentColor" />
+        <g className="x axis" transform={`translate(0, ${height})`}>
+          <path d={`M0,6V0H${width}V6`} fill="none" />
           {xTicks.map(([label, x]) => (
-            <g key={x} className="tick" opacity={1} transform={`translate(${x}, 0)`}>
-              <line y2={6} stroke="currentColor" />
-              <text y={9} dy="0.7em" fill="currentColor">
+            <g key={x} className="tick" transform={`translate(${x}, 0)`}>
+              <line y2={6} />
+              <text y={9} dy="0.7em" stroke="none">
                 {label}
               </text>
             </g>
           ))}
         </g>
-        <g className="y axis" fill="none" textAnchor="end">
-          <path className="domain" stroke="currentColor" d={`M-6,${height}H0V0H-6`} />
+        <g className="y axis" textAnchor="end">
+          <path d={`M-6,${height}H0V0H-6`} fill="none" />
           {yTicks.map(([label, y]) => (
-            <g key={y} className="tick" opacity={1} transform={`translate(0, ${y})`}>
-              <line x2={-6} stroke="currentColor" />
-              <text fill="currentColor" x={-9} dy="0.32em">
+            <g key={y} className="tick" transform={`translate(0, ${y})`}>
+              <line x2={-6} />
+              <text dy="0.32em" stroke="none" x={-9}>
                 {label}
               </text>
             </g>
           ))}
         </g>
         {r && (
-          <g opacity={0.3}>
+          <g className="bands" opacity={0.3} stroke="none">
             <path d={`M0 ${r.min0}V${r.min1}H${width}V${r.min0}Z`} fill="#EE0" />
             <path d={`M0 ${r.max0}V${r.max1}H${width}V${r.max0}Z`} fill="#EE0" />
             <path d={`M0 ${r.min1}V${r.max1}H${width}V${r.min1}Z`} fill="#0E0" />
           </g>
         )}
-        <g stroke="forestgreen" fill="forestgreen">
+        <g className="data" stroke="forestgreen" fill="forestgreen">
           <path d={line(data)!} fill="none" strokeWidth={2} />
           {data.map((d) => (
-            <circle cx={xScale(d.createdAt)} cy={yScale(d.ph)} key={d.id} r={4} />
+            <circle cx={xScale1(d.createdAt)} cy={yScale1(d.ph)} key={d.id} r={4} />
           ))}
         </g>
         {data.length === 0 && (
-          <text y={height / 2} x={width / 2} textAnchor="middle">
-            No Data To Display
-          </text>
+          <g className="no-data">
+            <text y={height / 2} x={width / 2} stroke="none">
+              No Data To Display
+            </text>
+          </g>
         )}
       </g>
     </svg>
